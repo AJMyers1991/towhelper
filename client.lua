01d4a139464f296b58f46eb502412513c8e615f5
AJMyers1991 <109767845+AJMyers1991@users.noreply.github.com>
2022-08-04 09:39:16 -0500

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,57 @@
+local useboom = 0
+
+local function IsPlayerInTowTruck()
+	local ped = PlayerPedId()
+	local tvehicle = GetVehiclePedIsIn(ped)
+	local tow1 = GetHashKey(Config.rollback1)
+	local tow2 = GetHashKey(Config.rollback2)
+	local tow3 = GetHashKey(Config.heavywrecker)
+	return IsVehicleModel(tvehicle, tow1) or IsVehicleModel(tvehicle, tow2) or IsVehicleModel(tvehicle, tow3)
+end
+
+local function IsPlayerInHeavyWrecker()
+	local ped = PlayerPedId()
+	local hvehicle = GetVehiclePedIsIn(ped)
+	local tow4 = GetHashKey(Config.heavywrecker)
+	return IsVehicleModel(hvehicle, tow4)
+end
+
+local function VehicleSpeed()
+	local ped = PlayerPedId()
+	local svehicle = GetVehiclePedIsIn(ped)
+	return GetEntitySpeed(svehicle)
+end
+
+CreateThread(function()
+    while true do
+		Wait(0)
+		local ped = PlayerPedId()
+		if Config.RollbackDisable == true and IsPlayerInTowTruck() and VehicleSpeed() > 1.0 then
+			DisableControlAction(2, 60, true)
+			DisableControlAction(2, 61, true)
+			DisableControlAction(2, 62, true)
+			DisableControlAction(2, 110, true)
+			DisableControlAction(2, 111, true)
+			DisableControlAction(2, 112, true)
+			DisableControlAction(2, 126, true)
+			DisableControlAction(2, 127, true)
+			DisableControlAction(2, 128, true)
+			DisableControlAction(2, 280, true)
+			DisableControlAction(2, 281, true)
+			DisableControlAction(2, 340, true)
+			DisableControlAction(2, 341, true)
+			DisableControlAction(2, 343, true)
+		end
+		if Config.FoxWrecker == true and IsPlayerInHeavyWrecker() then
+			if useboom == 0 and IsControlJustReleased(0, Config.EngageRotatorKey) then
+				Wait(5000)
+				SetCurrentPedVehicleWeapon(ped, -764006018)
+				useboom = 1
+			elseif useboom == 1 and IsControlJustReleased(0, Config.EngageRotatorKey) then
+				SetCurrentPedWeapon(ped, -1569615261, false)
+				useboom = 0
+			end
+		end
+    end
+end)
+
---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.RollbackDisable = true           --toggle to turn on and off the function to disable the rollback bed while vehicle is in motion
+Config.rollback1 = 'f550rbc'            --spawn name for your first rollback
+Config.rollback2 = '20ramrbc'           --spawn name for your second rollback
+
+Config.FoxWrecker = true                --only set to true if you are using the peterbilt rotator heavy wrecker by Foxtrot Delta
+Config.heavywrecker = 'foxwrecker'      --spawn name for foxtrot delta heavy wrecker
+Config.EngageRotatorKey = 101           --Key to turn on and off the rotator boom function on foxwrecker only
