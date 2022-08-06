14c428ec18292f4b053fe5af518c7bd9572fb4a7
AJMyers1991 <109767845+AJMyers1991@users.noreply.github.com>
2022-08-06 10:56:14 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,8 @@ Config = {}
 Config.RollbackDisable = true           --toggle to turn on and off the function to disable the rollback bed while vehicle is in motion
 Config.rollback1 = 'f550rbc'            --spawn name for your first rollback
 Config.rollback2 = '20ramrbc'           --spawn name for your second rollback
+Config.rollback3 = '20ramrbc'           --spawn name for your third rollback
+Config.rollback4 = '20ramrbc'           --spawn name for your fourth rollback
 
 Config.FoxWrecker = true                --only set to true if you are using the peterbilt rotator heavy wrecker by Foxtrot Delta
 Config.heavywrecker = 'foxwrecker'      --spawn name for foxtrot delta heavy wrecker
